61eb0e6de27f2d873142af744388f9626cda7295
Marcus Gabriel Sammon <marcus.g.sammon@protonmail.com>
2022-10-18 13:02:09 -0400

    Added ScaleConsonanceModelTest and minor modifications to NoteConsonanceModelTest

--- django2java/src/test/java/chord/relations/ScaleConsonanceModelTest.java
+++ django2java/src/test/java/chord/relations/ScaleConsonanceModelTest.java
@@ -15,28 +15,33 @@ import org.junit.jupiter.params.provider.MethodSource;
 import chord.ConsonanceRating;
 import chord.Interval;
 import chord.ident.ChordSignature;
+import chord.ident.ScaleSignature;
 
-public class NoteConsonanceModelTest {
-
+/**
+ * Test suite for the ScaleConsonanceModel.
+ * @author DAD
+ *
+ */
+public class ScaleConsonanceModelTest {
 	/**
 	 * Parameters for testing null pointer detection in add method.
 	 * @return stream of arguments containing one null argument for addRatingTest
 	 */
 	static Stream<Arguments> oneNullArgumentForAdd(){
 		return Stream.of(
-				Arguments.of(null,Interval.MINOR2,ConsonanceRating.MEDIOCRE),
+				Arguments.of(null,ScaleSignature.AEOLIAN,ConsonanceRating.MEDIOCRE),
 				Arguments.of(ChordSignature.MAJOR,null,ConsonanceRating.MEDIOCRE),
-				Arguments.of(ChordSignature.MAJOR,Interval.MINOR2,null)
+				Arguments.of(ChordSignature.MAJOR,ScaleSignature.AEOLIAN_b5,null)
 				);
 	}
 
 	/**
-	 * Parameters for testing null pointer detection in remove method.
+	 * Parameters for testing null pointer detection in remove and get method.
 	 * @return stream of arguments containing one null argument for removeRatingTest
 	 */
 	static Stream<Arguments> oneNullArgumentForRemoveAndGet(){
 		return Stream.of(
-				Arguments.of(null,Interval.MINOR2),
+				Arguments.of(null,ScaleSignature.AEOLIAN_BLUES),
 				Arguments.of(ChordSignature.MAJOR,null)
 				);
 	}
@@ -48,7 +53,7 @@ public class NoteConsonanceModelTest {
 	 */
 	static Stream<Arguments> defaultArgumentsToReduceCode(){
 		return Stream.of(
-				Arguments.of(ChordSignature.MAJOR,Interval.MINOR2,ConsonanceRating.BAD)
+				Arguments.of(ChordSignature.MAJOR,ScaleSignature.AEOLIAN_NATURAL_7,ConsonanceRating.BAD)
 				);
 	}
 
@@ -56,7 +61,7 @@ public class NoteConsonanceModelTest {
 	 * Main data model used for tests.
 	 * Initialized before every test.
 	 */
-	NoteConsonanceModel ncModel;
+	ScaleConsonanceModel scModel;
 
 	/**
 	 * Initialization code for main data model.
@@ -64,37 +69,22 @@ public class NoteConsonanceModelTest {
 	 */
 	@BeforeEach
 	void init() {
-		ncModel = new NoteConsonanceModel();
+		scModel = new ScaleConsonanceModel();
 	}
 
 	/**
 	 * Ensure that the addRating method throws an exception when
 	 * passed a null value for each of the parameters
 	 * @param chordSig Chord Signature
-	 * @param interval interval between the root of the chord and the note
+	 * @param scaleSig Scale Signature
 	 * @param rating rating for the chord change.
 	 */
 	@ParameterizedTest
 	@MethodSource("oneNullArgumentForAdd")
-	void testAddNull(ChordSignature chordSig, Interval interval, ConsonanceRating rating) {
+	void testAddNull(ChordSignature chordSig, ScaleSignature scaleSig, ConsonanceRating rating) {
 		Exception exception = 
 				assertThrows(NullPointerException.class, 
-						() -> ncModel.addRating(chordSig, interval, rating));
-	}
-
-	/**
-	 * Ensure that the data model throws an exception when attempting
-	 * to add a rating not in the first octave.
-	 * (All notes in second octave are duplicates of first)
-	 */
-	@Test
-	void testAddNoteRatingIntervalNotInFirstOctave() {
-		Exception exception = 
-				assertThrows(IllegalArgumentException.class, 
-						() -> ncModel.addRating(
-								ChordSignature.b2, 
-								Interval.PERFECT8, 
-								ConsonanceRating.MEDIOCRE));
+						() -> scModel.addRating(chordSig, scaleSig, rating));
 	}
 
 	/**
@@ -103,19 +93,15 @@ public class NoteConsonanceModelTest {
 	@Test
 	void testAddAllValidConsonanceRating() {
 		for(ChordSignature chordSig : ChordSignature.values()) {
-			for(Interval interval : Interval.values()) {
-				if( !interval.inFirstOctave() ) {
-					break;
-				}
-
+			for(ScaleSignature scaleSig : ScaleSignature.values()) {
 				//adding a rating is trivial so no reason to check every
 				//rating
 				final ConsonanceRating firstRatingAdded = ConsonanceRating.VERY_GOOD;
 				//Make sure that the data model is following
 				//the contract, return null on first addition,
 				//return last rating on second addition
-				assertNull(ncModel.addRating(chordSig, interval, firstRatingAdded));
-				assertEquals(firstRatingAdded, ncModel.addRating(chordSig,interval, ConsonanceRating.VERY_BAD));
+				assertNull(scModel.addRating(chordSig, scaleSig, firstRatingAdded));
+				assertEquals(firstRatingAdded, scModel.addRating(chordSig,scaleSig, ConsonanceRating.VERY_BAD));
 			}
 		}
 	}
@@ -124,101 +110,71 @@ public class NoteConsonanceModelTest {
 	 * Ensure that the remove method throws an exception if passed a
 	 * null argument.
 	 * @param chordSig Chord Signature
-	 * @param interval interval between the root of the chord and the note
+	 * @param scaleSig Scale Signature
 	 */
 	@ParameterizedTest
 	@MethodSource("oneNullArgumentForRemoveAndGet")
-	void testRemoveNull(ChordSignature chordSig, Interval interval) {
+	void testRemoveNull(ChordSignature chordSig, ScaleSignature scaleSig) {
 		Exception exception = 
 				assertThrows(NullPointerException.class, 
-						() -> ncModel.removeRating(chordSig, interval));
-	}
-
-	/**
-	 * Ensure that the model throws an exception for intervals not in
-	 * the first octave.
-	 * (All intervals in second octave are duplicate notes of the first)
-	 */
-	@Test
-	void testRemoveNoteRatingIntervalNotInFirstOctave() {
-		Exception exception = 
-				assertThrows(IllegalArgumentException.class, 
-						() -> ncModel.removeRating(
-								ChordSignature.b2, 
-								Interval.PERFECT8));
+						() -> scModel.removeRating(chordSig, scaleSig));
 	}
 
 	/**
 	 * Ensure that a rating can be removed successfully when it exists.
 	 * @param chordSig Chord Signature
-	 * @param interval interval between the root of the chord and the note
-	 * @param rating rating for the chord change
+	 * @param scaleSig Scale Signature
+	 * @param rating rating for the chord/scale combination
 	 */
 	@ParameterizedTest
 	@MethodSource("defaultArgumentsToReduceCode")
-	void testRemoveNoteRatingExistingRating(ChordSignature chordSig, Interval interval, ConsonanceRating rating) {
-		ncModel.addRating(chordSig, interval, rating);
-		assertEquals(rating, ncModel.removeRating(chordSig, interval));
-		assertNull(ncModel.removeRating(chordSig, interval));
+	void testRemoveScaleRatingExistingRating(ChordSignature chordSig, ScaleSignature scaleSig, ConsonanceRating rating) {
+		scModel.addRating(chordSig, scaleSig, rating);
+		assertEquals(rating, scModel.removeRating(chordSig, scaleSig));
+		assertNull(scModel.removeRating(chordSig, scaleSig));
 	}
 
 	/**
 	 * Ensure that the data model returns null when attempting to
 	 * remove a non existent rating
 	 * @param chordSig Chord Signature
-	 * @param interval interval between the root of the chord and the note
+	 * @param scaleSig Scale Signature
 	 * @param rating rating for the chord change.
 	 */
 	@ParameterizedTest
 	@MethodSource("defaultArgumentsToReduceCode")
-	void testRemoveNoteRatingNonExistentRating(ChordSignature chordSig, Interval interval, ConsonanceRating rating) {
-		assertNull(ncModel.removeRating(chordSig, interval));
+	void testRemoveScaleRatingNonExistentRating(ChordSignature chordSig, ScaleSignature scaleSig, ConsonanceRating rating) {
+		assertNull(scModel.removeRating(chordSig, scaleSig));
 	}
 
 	/**
 	 * Ensure that the get method throws an exception if passed a
 	 * null argument.
 	 * @param chordSig Chord Signature
-	 * @param interval interval between the root of the chord and the note
+	 * @param scaleSig Scale Signature
 	 */
 	@ParameterizedTest
 	@MethodSource("oneNullArgumentForRemoveAndGet")
-	void testGetRatingPassedNull(ChordSignature chordSig, Interval interval) {
+	void testGetRatingPassedNull(ChordSignature chordSig, ScaleSignature scaleSig) {
 		Exception exception = 
 				assertThrows(NullPointerException.class, 
-						() -> ncModel.getRating(chordSig, interval));
-	}
-
-
-	/**
-	 * Ensure that the data model throws an exception when attempting to
-	 * use an interval beyond the first octave
-	 * (All intervals beyond the first octave are duplicate notes.)
-	 */
-	@Test
-	void testGetRatingPassedInvalidInterval() {
-		Exception exception = 
-				assertThrows(IllegalArgumentException.class, 
-						() -> ncModel.getRating(
-								ChordSignature.b2,  
-								Interval.PERFECT8));
+						() -> scModel.getRating(chordSig, scaleSig));
 	}
 
 	/**
 	 * Ensure that the data model obeys the contract for retrieving
 	 * ratings. null if rating doesn't exist, the rating if it does exist.
 	 * @param chordSig Chord Signature
-	 * @param interval interval between the root of the chord and the note
+	 * @param scaleSig Scale Signature
 	 * @param rating rating for the interval.
 	 */
 	@ParameterizedTest
 	@MethodSource("defaultArgumentsToReduceCode")
-	void testGetRatingPassedValidValues(ChordSignature chordSig, Interval interval, ConsonanceRating rating) {
-		assertNull(ncModel.getRating(chordSig, interval));
+	void testGetRatingPassedValidValues(ChordSignature chordSig, ScaleSignature scaleSig, ConsonanceRating rating) {
+		assertNull(scModel.getRating(chordSig, scaleSig));
 
-		ncModel.addRating(chordSig, interval, rating);
+		scModel.addRating(chordSig, scaleSig, rating);
 
-		assertEquals(rating, ncModel.getRating(chordSig, interval));
+		assertEquals(rating, scModel.getRating(chordSig, scaleSig));
 	}
-
 }

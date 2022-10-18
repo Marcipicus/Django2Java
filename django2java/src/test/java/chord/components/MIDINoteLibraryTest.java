eea067778a06efe18bbdfe04015a343d14ce9a3a
Marcus Gabriel Sammon <marcus.g.sammon@protonmail.com>
2022-10-18 09:51:46 -0400

    Reorganizing test packages and removing old/unnecessary files

--- django2java/src/test/java/chord/components/MIDINoteLibraryTest.java
+++ django2java/src/test/java/chord/components/MIDINoteLibraryTest.java
@@ -1,4 +1,4 @@
-package chord.components.test;
+package chord.components;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
